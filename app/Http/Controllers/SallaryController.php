457cc04b576923e674841e9f605354ea23a8695f
hdmax25 <47875083+hdmax25@users.noreply.github.com>
2020-06-07 17:24:44 +0700

    Create SallaryController.php

---
+++ app/Http/Controllers/SallaryController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SallaryController extends Controller
+{
+    public function index() {
+
+    }
+
+    public function update() {
+
+    }
+}
